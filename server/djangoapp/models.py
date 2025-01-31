915eaba6e07a3dea7d5e2d4759d67323dfa719d3
MarcisB8 <68816303+MarcisB8@users.noreply.github.com>
2025-01-31 15:06:00 +0200

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -13,8 +13,8 @@ class CarMake(models.Model):
         ('LUXURY', 'Luxury')
     ]
     brand_level = models.CharField(
-        max_length=10, 
-        choices=BRAND_LEVEL, 
+        max_length=10,
+        choices=BRAND_LEVEL,
         default='MEDIUM'
     )
 
@@ -28,7 +28,8 @@ class CarModel(models.Model):
         validators=[
             MaxValueValidator(100),
             MinValueValidator(1)
-        ])
+        ]
+    )
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
@@ -67,20 +68,38 @@ class CarModel(models.Model):
         ('RESERVED', 'Reserved'),
         ('SOLD', 'Sold')
     ]
-    status = models.CharField(max_length=10, choices=CAR_STATUS, default='COMING')
-    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
+    status = models.CharField(
+        max_length=10,
+        choices=CAR_STATUS,
+        default='COMING'
+    )
+    type = models.CharField(
+        max_length=10,
+        choices=CAR_TYPES,
+        default='SUV'
+    )
     year = models.IntegerField(default=2023,
         validators=[
             MaxValueValidator(2023),
             MinValueValidator(2015)
-        ])
-    fuel = models.CharField(max_length=10, choices=CAR_FUEL, default='GASOLINE')
+        ]
+    )
+    fuel = models.CharField(
+        max_length=10,
+        choices=CAR_FUEL,
+        default='GASOLINE'
+    )
     engine = models.FloatField(default=2.0,
         validators=[
             MaxValueValidator(10.0),
             MinValueValidator(0.5)
-        ])
-    color = models.CharField(max_length=10, choices=CAR_COLOR, default='WHITE')
+        ]
+    )
+    color = models.CharField(
+        max_length=10,
+        choices=CAR_COLOR,
+        default='WHITE'
+    )
 
     def __str__(self):
         return self.name  # Return the name as the string representation
