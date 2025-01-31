b6b8ff5bc1f30a3db2d44ebe16ff534723af2e87
MarcisB8 <68816303+MarcisB8@users.noreply.github.com>
2025-01-31 15:47:29 +0200

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -138,11 +138,11 @@ def add_review(request):
         data = json.loads(request.body)
         try:
             response = post_review(data)
-            return JsonResponse({"status": 200, "review": data})
+            return JsonResponse({"status": 200, "review": response})
         except Exception as e:
+            print(f"Error: {e}")
             return JsonResponse({
                 "status": 401, "message": "Error in posting review"
             })
-            print(f"Error: {e}")
     else:
         return JsonResponse({"status": 403, "message": "Unauthorized"})
